5ea2d95723baff2bcd43c8ca8d234a601945ed88
Caia Chuang <caiachung@gmail.com>
2020-08-03 23:23:45 +0000

    Adding a Second Route and Template

--- express_server.js
+++ express_server.js
@@ -4,16 +4,20 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-
-
-app.get("/urls", (req, res) => {
 	const urlDatabase = {
 		"b2xVn2": "http://www.lighthouselabs.ca",
 		"9sm5xK": "http://www.google.com"
 	};
 
+app.get("/urls", (req, res) => {
 	let templateVars = {urls: urlDatabase};
+
 	res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {

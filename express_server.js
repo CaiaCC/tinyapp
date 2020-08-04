1fc26775a22ee03b72081f7fadabeb260525e666
Caia Chuang <caiachung@gmail.com>
2020-08-04 23:35:54 +0000

    enable editing long URL

--- express_server.js
+++ express_server.js
@@ -3,21 +3,20 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extend: true}));
-
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-	const alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	let outputShortURL = "";
-	for (let i = 0; i< 6; i++) {
-		outputShortURL += alphanumeric[Math.floor(Math.random() * alphanumeric.length)];
-	};
-	return outputShortURL;
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  let randomURL = "";
+  for (let i = 0; i< 6; i++) {
+    randomURL += alphanumeric[Math.floor(Math.random() * alphanumeric.length)];
+  };
+  return randomURL;
 }
 
 const urlDatabase = {
-	"b2xVn2": "http://www.lighthouselabs.ca",
-	"9sm5xK": "http://www.google.com"
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
 app.get("/urls/new", (req, res) => {
@@ -25,59 +24,73 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-	const shortURL = req.params.shortURL;
+  const shortURL = req.params.shortURL;
 
-	delete urlDatabase[shortURL];
-
+  delete urlDatabase[shortURL];
   res.redirect("/urls")
 });
 
+app.post("/urls/:id", (req, res) => {
+  const shortURL= req.params.id;
+  let longURL = req.body.longURL;
+  
+  if (longURL.length === 0) {
+    return res.redirect("/urls");
+  }
+  if (!longURL.includes("http://" )) {
+    longURL = "http://" + longURL;
+  }
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-	const shortURL = req.params.shortURL;
-	const longURL = urlDatabase[shortURL] ;
-	
-	let templateVars = {shortURL, longURL};
-	
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL] ;
+  let templateVars = {shortURL, longURL};
+  
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-	let longURL = urlDatabase[req.params.shortURL];
-	if (!longURL.includes("http://" )) {
-		res.redirect("http://" + longURL);
-	}
-	res.redirect(longURL);
+  let longURL = urlDatabase[req.params.shortURL];
+
+  if (!longURL.includes("http://" )) {
+    longURL = "http://" + longURL;
+  }
+  res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
-	let templateVars = {urls: urlDatabase};
-	
-	res.render("urls_index", templateVars);
+  let templateVars = {urls: urlDatabase};
+  
+  res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-	let longURL = req.body.longURL;
-	const shortURL = generateRandomString();
-	
-	if (!longURL.includes("http://" )) {
-		longURL = "http://" + longURL;
-	}
-	let urlDatabaseKeys = Object.keys(urlDatabase);
-	for (let key of urlDatabaseKeys) {
-		if (urlDatabase[key] === longURL) {
-			return res.redirect("/urls/" + key);
-		} 
-	}
-	urlDatabase[shortURL] = longURL;
-	return res.redirect("/urls/" + shortURL);
+  let longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  
+  if (!longURL.includes("http://" )) {
+    longURL = "http://" + longURL;
+  }
+  let urlDatabaseKeys = Object.keys(urlDatabase);
+  
+  for (let key of urlDatabaseKeys) {
+    if (urlDatabase[key] === longURL) {
+      return res.redirect("/urls/" + key);
+    } 
+  }
+  urlDatabase[shortURL] = longURL;
+  return res.redirect("/urls/" + shortURL);
 });
 
 app.get("/", (req, res) => {
-	res.send("How you doing?");
+  res.send("How you doing?");
 });
 
 app.listen(PORT, () => {
-	console.log(`Example app listening to on port ${PORT}`);
+  console.log(`Example app listening to on port ${PORT}`);
 });
 
 
@@ -86,12 +99,12 @@ app.listen(PORT, () => {
 /*
 let reqLongURL = req.body.longURL;
 for (let key of urlDatabaseKeys) {
-	if (urlDatabase[key] === reqLongURL) {
-		res.send("It already has a short URL")
-	} else {
-		const reqShortURL = generateRandomString();
-		urlDatabase[reqShortURL] = reqLongURL
-	}
+  if (urlDatabase[key] === reqLongURL) {
+    res.send("It already has a short URL")
+  } else {
+    const reqShortURL = generateRandomString();
+    urlDatabase[reqShortURL] = reqLongURL
+  }
 }
 */
 
@@ -105,12 +118,12 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/set", (req, res) => {
-	const a = 1;
-	res.send(`a = ${a}`);
+  const a = 1;
+  res.send(`a = ${a}`);
  });
  
  app.get("/fetch", (req, res) => {
-	res.send(`a = ${a}`);
+  res.send(`a = ${a}`);
  });
 */
 

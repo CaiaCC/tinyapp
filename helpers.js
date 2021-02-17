66bf6365be88e2819ec757e982f839538f0248ba
Caia Chuang <caiachuang@gmail.com>
2021-02-16 17:26:27 -0800

    Create a helper function: shortURLGenerator.

---
+++ helpers.js
@@ -0,0 +1,14 @@
+const shortURLGenerator = () => {
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    let randomString = "";
+
+    for (let i = 0; i < 6; i++) {
+        randomString += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return randomString;
+};
+
+module.exports = { shortURLGenerator }
